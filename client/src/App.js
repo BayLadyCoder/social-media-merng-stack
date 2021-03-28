3b151470bd632641ded8e8ed6555efb8e4d51ed7
BayLadyCoder <rachada_32@hotmail.com>
2021-03-28 14:55:34 -0400

    Added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Menu, Segment, Container, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
+
+  const handleItemClick = (e, name) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary size="massive" color="teal">
+      <Container>
+        <Menu.Item
+          name="home"
+          onClick={(e) => handleItemClick(e, "home")}
+          as={Link}
+          to="/"
+        >
+          <Icon name="home" />
+        </Menu.Item>
+
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={(e) => handleItemClick(e, "home")}
+            as={Link}
+            to="/"
+          />
+          <Menu.Item
+            name="login"
+            active={activeItem === "login"}
+            onClick={(e) => handleItemClick(e, "login")}
+            as={Link}
+            to="/login"
+          />
+          <Menu.Item
+            name="register"
+            active={activeItem === "register"}
+            onClick={(e) => handleItemClick(e, "register")}
+            as={Link}
+            to="/register"
+          />
+        </Menu.Menu>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
