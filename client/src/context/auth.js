b7e3170ec458ce6fa6cd34848afd7a3036ebb6cf
BayLadyCoder <rachada_32@hotmail.com>
2021-03-28 22:10:46 -0400

    Store token in local storage

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,8 +1,25 @@
 import React, { createContext, useReducer } from "react";
+import jwtDecode from "jwt-decode";
 
 // Action Types
 const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
+
+const initialState = {
+  user: null,
+};
+
+// Check if there is a token and the token is valid
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+
+  // if the token is expired
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
 
 // state
 const AuthContext = createContext({
@@ -23,13 +40,17 @@ const authReducer = (state, action) => {
 };
 
 const AuthProvider = (props) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null }); // reducer, initialState
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
   const login = (userData) => {
+    // save token to local storage
+    localStorage.setItem("jwtToken", userData.token);
     dispatch({ type: LOG_IN, payload: userData });
   };
 
   const logout = () => {
+    // remove token from local storage
+    localStorage.removeItem("jwtToken");
     dispatch({ type: LOG_OUT });
   };
 
