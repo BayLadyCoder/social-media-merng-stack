211e8b19d0046fc27e6c1fe64d7bdc7ce5c98d0d
BayLadyCoder <rachada_32@hotmail.com>
2021-04-03 22:54:17 -0400

    Added Confirm modal onDelete Post

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,15 +1,18 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Confirm } from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
 import { MUTATION_DELETE_POST, QUERY_GET_POSTS } from "../util/graphql";
 
 const DeleteButton = ({ postId }) => {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
   const [deletePost] = useMutation(MUTATION_DELETE_POST, {
     variables: { postId },
     onError(err) {
       console.log(err);
     },
     update(cache, result) {
+      setConfirmOpen(false);
       const data = cache.readQuery({ query: QUERY_GET_POSTS });
       cache.writeQuery({
         query: QUERY_GET_POSTS,
@@ -22,13 +25,20 @@ const DeleteButton = ({ postId }) => {
   });
 
   return (
-    <Button
-      as="div"
-      color="red"
-      onClick={() => deletePost()}
-      icon="trash"
-      floated="right"
-    />
+    <>
+      <Button
+        as="div"
+        color="red"
+        onClick={() => setConfirmOpen(true)}
+        icon="trash"
+        floated="right"
+      />
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={() => deletePost()}
+      />
+    </>
   );
 };
 
